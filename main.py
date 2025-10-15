810d76c54e9b28eef0499e0e2354d5407defa8f1
Alexander Nikolas Reuber <info@unconnect.net>
2025-10-15 22:33:25 +0200

    Add error handling

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
 
 
 load_dotenv()
@@ -11,12 +12,24 @@ client = genai.Client(api_key=api_key)
 
 def main():
 
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    script_name = sys.argv[0]
+    args = sys.argv[1:]
+
+    # FIXME: Before commit make this more pythionic with "not" keyword. 
+    # FIXME: Give an example how to use the cli-tool
+    if(len(args) < 1):
+        raise SystemExit("Something went wrong! Please give at least one argument!")
+    
+    # FIXME: Join possible multiple args when not encased in quotes to one arg.
+    prompt = args[0]
+
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
 
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
+    return 0
 
 if __name__ == "__main__":
     main()
