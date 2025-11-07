1335ccedffb13a77fbff9bc7722a7e1179739f8f
Alexander Nikolas Reuber <info@unconnect.net>
2025-11-07 18:28:27 +0100

    add llm func call basics

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,5 @@
 import os
+from google.genai import types
 
 def get_files_info(working_directory, directory="."):
   relative_path = os.path.join(working_directory, directory)
@@ -21,3 +22,18 @@ def get_files_info(working_directory, directory="."):
       return "\n".join(entries_infos)
       
   return f"Result for current directory:\n{compute_entry_infos()}"
+
+# Function schema: Tells the LLM how to use the function.
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 import sys
+from functions.get_files_info import schema_get_files_info
 
 def main():
 
@@ -21,22 +22,47 @@ def main():
         return sys.exit(1)
     
     # Join arguments in case the user does not enclose prompt in quotes
-    user_prompt = args[0]
-    system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
+    user_prompt = " ".join(args)
+    
+    # The system prompt is passed to the LLM, so that the user can't do "anything"
+    system_prompt = """
+    You are a helpful AI coding agent.
+
+    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+    - List files and directories
+
+    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """
 
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
+    # All the functions we wanna allow the LLM to use.
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )
+
     # Send content to LLM and get response
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt)
+        config=types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction=system_prompt)
         )
+    
+    # Print function calls
+    if response.function_calls is not None:
+        for call in response.function_calls:
+            print(f"Calling function: {call.name}({call.args})")
+    else:     
+        # Print LLM text response
+        print(response.text)
 
-    # Print LLM response
-    print(response.text)
     
     # Output verbose response and token usage
     if "--verbose" in args:
