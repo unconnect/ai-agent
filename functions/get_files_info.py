c8474198630121cf763aca19490be853c154c5e1
Alexander Nikolas Reuber <info@unconnect.net>
2025-11-07 10:54:33 +0100

    refactor test and fix get_files_info check

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -4,17 +4,9 @@ def get_files_info(working_directory, directory="."):
   absolute_path = os.path.abspath(relative_path)
   work_dir_content = os.listdir(absolute_path)
 
-  if directory not in absolute_path:
+  if working_directory not in absolute_path:
     error = f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-    # print(error)
     return error
-
-  # print("_______DEBUG_______")
-  # print(f"working_directory, directory: {working_directory}, {directory}")
-  # print(f"relative_path: {relative_path}")
-  # print(f"absolute_path: {absolute_path}")
-  # print(f"work_dir_content: {work_dir_content}")
-  # print("_______DEBUG_______")
 
   def compute_entry_infos():
       entries_infos = []
@@ -27,5 +19,4 @@ def get_files_info(working_directory, directory="."):
 
       return "\n".join(entries_infos)
       
-
   return f"Result for current directory:\n{compute_entry_infos()}"
--- tests.py
+++ tests.py
@@ -1,26 +1,11 @@
-import unittest
 from functions.get_files_info import get_files_info
 
-class GetFileInfo(unittest.TestCase):
-  def setUp(self):
-    self.get_files_info = get_files_info
-
-  def test_calculator(self):
-    result = self.get_files_info("calculator", ".")
-    self.assertEqual(result, 
-    "Result for current directory:"
-    "\n - main.py: file_size=729 bytes, is_dir=False"
-    "\n - pkg: file_size=160 bytes, is_dir=True"
-    "\n - tests.py: file_size=1342 bytes, is_dir=False"
-    )
-
-  def test_slash_bin(self):
-    result = self.get_files_info("calculator", "..")
-    self.assertEqual(result, 
-    """
-    Result for '/bin' directory:
-    Error: Cannot list "/bin" as it is outside the permitted working directory
-    """)
+def main():
+  print(get_files_info("calculator", "."))
+  print(get_files_info("calculator", "pkg"))
+  print(get_files_info("calculator", "/bin"))
+  print(get_files_info("calculator", "../"))
+  
 
 if __name__ == "__main__":
-    unittest.main()
+    main()
